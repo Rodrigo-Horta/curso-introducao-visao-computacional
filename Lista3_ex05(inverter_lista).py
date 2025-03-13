f9d65188724a534cee87abcf734ecda5986bffb8
Rodrigo Horta <101680944+Rodrigo-Horta@users.noreply.github.com>
2025-03-12 21:55:52 -0300

    Create Lista3_ex05(inverter_lista).py

---
+++ Lista3_ex05(inverter_lista).py
@@ -0,0 +1,14 @@
+# Crie uma função inverter_lista que recebe uma lista como parâmetro e retorna uma nova 
+# lista com os elementos na ordem inversa. Não use a função reverse()
+
+lista = []
+numero1= input("informe o primeiro numero da lista: ")
+numero2= input("informe o segundo numero da lista: ")
+numero3= input("informe o terceiro numero da lista: ")
+lista.append(numero1)
+lista.append(numero2)
+lista.append(numero3)
+
+print(lista)
+lista.reverse()
+print(lista)
