d7afd3cfb68b5fc8cbabd57ad1c88a9ab13f8694
Rodrigo Horta <101680944+Rodrigo-Horta@users.noreply.github.com>
2025-03-12 21:40:17 -0300

    Add files via upload

---
+++ Lista1_ex02.py
@@ -0,0 +1,9 @@
+idade=input("Quantos anos vc tem?")
+idade=int(idade)
+dias=idade*365
+print(f"Então sua idade aproximada em dias é: {dias: .0f} dias")
+
+#Calculadora de Idade em Dias
+#Peça a idade do usuário em anos
+#Calcule quantos dias aproximados a pessoa já viveu (desconsiderando anos bissextos)
+#Mostre o resultado
